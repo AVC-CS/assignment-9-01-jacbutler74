7a2c50eb52dd7b1b5d1f20e05c5dc2725184c21d
jacbutler74 <jacbutler74@gmail.com>
2024-02-25 06:39:16 -0800

    finished main.hpp

--- main.hpp
+++ main.hpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <fstream>
+#include <string>
 using namespace std;
 
 const int N = 5;
@@ -13,18 +14,40 @@ struct Student
 };
 
 void makeStudent(Student *student);
-void printStudent(Student *head);
+void printStudent(Student *student);
 
 void makeStudent(Student *student)
 {
-    /*******************************
-     * Code your program here
-     *******************************/
+    ifstream ifs;
+
+    string temp;
+    int count = 0, index = 0;
+
+    ifs.open("students.txt");
+
+    
+    while(ifs >> temp) {
+        switch (count) {
+            case 0:
+                student[index].id = stoi(temp);
+                ++count;
+                break;
+            case 1:
+                student[index].name = temp;
+                ++count;
+                break;
+            case 2:
+                student[index].major = temp;
+                count = 0;
+                ++index;
+                break;
+        }
+    }
 }
 void printStudent(Student *student)
 {
     cout << endl;
-    /*******************************
-     * Code your program here
-     *******************************/
+    for (int i = 0; i < N; ++i) {
+        cout << student[i].id << " " << student[i].name << " " << student[i].major << endl;
+    }
 }
